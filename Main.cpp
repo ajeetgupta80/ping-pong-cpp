08b07030dedc6e73085d953ae0a4ca4539d8cf19
fucking ajeet <ajeet808080@gmail.com>
2022-07-27 00:10:32 +0530

    1st day

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include"iostream"
+using namespace std;
+
+enum eDir {STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT =3, RIGHT =4, UPRIGHT =5, DOWNRIGHT =6};
+
+class cBall
+{
+    private:
+    // position of the ball
+    int x,y;
+    int originalX, originalY;
+    eDir direction;
+
+    public:
+     cBall(int posX, int posY)
+     {
+        
+     }
+    
+
+}
+
+
+
+int main()
+{
+  
+}
